47c2dcfc6f0a0c3c6cf7a973e81c358796c31fc6
malBeshaba <1175245836@qq.com>
2020-04-28 20:53:13 +0800

    项目创建

---
+++ src/main/java/com/example/mallsystem/MallsystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.mallsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MallsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mallsystem/MallsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mallsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
